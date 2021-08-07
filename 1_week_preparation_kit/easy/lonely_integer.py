99f06f3653341cad66eb106f64d766b3a80d22eb
cecilialiu <44141273+cecilialiu0803@users.noreply.github.com>
2021-08-07 14:37:22 +0100

    remove unneeded lines

--- 1_week_preparation_kit/easy/lonely_integer.py
+++ 1_week_preparation_kit/easy/lonely_integer.py
@@ -1,10 +1,3 @@
-#
-# Complete the 'lonelyinteger' function below.
-#
-# The function is expected to return an INTEGER.
-# The function accepts INTEGER_ARRAY a as parameter.
-#
-
 from collections import Counter
 
 
